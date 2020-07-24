4ee4928e75f1be876456217747a3fb356462d101
Liubov Glushanina <love@MacBook-Pro.local>
2020-07-24 15:52:29 +0200

    added all menu items to index.html

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Size
+from .models import *
 
 # Create your views here.
 def index(request):
     context = {
-        "sizes": Size.objects.all()
+        "sizes": Size.objects.all(),
+        "menu_items": Food.objects.all()
     }
     return render(request, "orders/index.html", context)
