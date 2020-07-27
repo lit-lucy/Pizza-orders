a8f7588afe2d501851a4e6db4ef14777e773c60b
Liubov Glushanina <love@MacBook-Pro.local>
2020-07-27 15:26:12 +0200

    added prices to menu page

--- orders/views.py
+++ orders/views.py
@@ -5,20 +5,10 @@ from .models import *
 
 # Create your views here.
 def index(request):
-    menu_items = {}
-    food = Food.objects.all()
-
-
-    for i in range(len(food)):
-        food_type = food[i].food_type.type_name
-        if food_type in menu_items:
-            menu_items[food_type].append(food[i].food_name)
-        else:
-            menu_items[food_type] = [food[i].food_name]
 
     context = {
-        "menu_items": menu_items,
-        "food": food,
-        "food_types": FoodType.objects.all()
+
+        "food_types": FoodType.objects.all(),
+        
     }
     return render(request, "orders/index.html", context)
