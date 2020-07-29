dc65a83d8fcce14b137173cf18685431c11ea6e6
Liubov Glushanina <love@MacBook-Pro.local>
2020-07-29 15:52:58 +0200

    added choose size and topping on modification page

--- orders/urls.py
+++ orders/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:food_id>", views.food_modifications, name="food_modifications"),
+    path('<int:food_id>/add_to_order/', views.add_to_order, name='add_to_order'),
 
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from .models import *
 
@@ -20,7 +21,13 @@ def food_modifications(request, food_id):
         raise Http404("Dish does not exist")
 
     context = {
-        "food": food
+        "food": food,
+        "range": range(food.number_of_extras),
     }
 
     return render(request, "orders/food.html", context)
+
+def add_to_order(request, food_id):
+    food = get_object_or_404(Food, pk=food_id)
+
+    return HttpResponse('done')
